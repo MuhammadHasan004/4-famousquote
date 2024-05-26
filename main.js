412488631bc565fa3ea8de5081ae8aaebe80b6c0
MuhammadHasan004 <hasanmuhammadhm1122@gmail.com>
2024-05-26 14:01:49 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,2 @@
+var quoteauthor = "Benjamin Franklin";
+console.log("".concat(quoteauthor, " said Three can keep a secret, if two of them are dead"));
---
+++ main.ts
@@ -0,0 +1,2 @@
+let quoteauthor:string="Benjamin Franklin";
+console.log(`${quoteauthor} said Three can keep a secret, if two of them are dead`);
